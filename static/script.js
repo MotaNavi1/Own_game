d6641812a0ef4c6b8b8f52a2d36be060a8f60e29
MotaNavi1 <163740982+MotaNavi1@users.noreply.github.com>
2025-06-01 21:38:53 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,42 +1,9 @@
-function createMediaElement(filename) {
-  const url = "/uploads/" + filename;
-  const div = document.createElement("div");
-  div.className = "media-item";
-
-  if (filename.match(/\.(jpg|jpeg|png|gif)$/i)) {
-    const img = document.createElement("img");
-    img.src = url;
-    div.appendChild(img);
-  } else if (filename.match(/\.(mp4|webm)$/i)) {
-    const video = document.createElement("video");
-    video.src = url;
-    video.controls = true;
-    div.appendChild(video);
-  }
-
-  return div;
-}
-
-function loadGallery() {
-  fetch("/media")
-    .then(res => res.json())
-    .then(files => {
-      const gallery = document.getElementById("gallery");
-      gallery.innerHTML = "";
-      files.forEach(file => {
-        const el = createMediaElement(file);
-        gallery.appendChild(el);
-      });
-    });
-}
-
 document.getElementById("uploadForm").addEventListener("submit", function(e) {
   e.preventDefault();
   const input = this.querySelector('input[type="file"]');
   const files = input.files;
   if (!files.length) return;
 
-  const formData = new FormData();
   for (let file of files) {
     if (file.type.startsWith("video/")) {
       const url = URL.createObjectURL(file);
@@ -48,25 +15,38 @@ document.getElementById("uploadForm").addEventListener("submit", function(e) {
         if (tempVideo.duration > 10) {
           alert("Видео должно быть не длиннее 10 секунд!");
         } else {
-          formData.append("file", file);
-          uploadFiles(formData);
+          showMedia(file);
         }
       };
     } else {
-      formData.append("file", file);
+      showMedia(file);
     }
   }
 
-  function uploadFiles(formData) {
-    fetch("/upload", {
-      method: "POST",
-      body: formData
-    }).then(() => {
-      loadGallery();
-      input.value = "";
-    });
-  }
+  input.value = "";
 });
 
-window.onload = loadGallery;
+function showMedia(file) {
+  const reader = new FileReader();
+  reader.onload = function(event) {
+    const url = event.target.result;
+    const gallery = document.getElementById("gallery");
+    const div = document.createElement("div");
+    div.className = "media-item";
+
+    if (file.type.startsWith("image/")) {
+      const img = document.createElement("img");
+      img.src = url;
+      div.appendChild(img);
+    } else if (file.type.startsWith("video/")) {
+      const video = document.createElement("video");
+      video.src = url;
+      video.controls = true;
+      div.appendChild(video);
+    }
+
+    gallery.prepend(div);
+  };
+  reader.readAsDataURL(file);
+}
 
